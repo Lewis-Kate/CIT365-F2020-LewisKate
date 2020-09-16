ca83b7c18bd11983399f7f55710c69113db36417
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-09-15 23:05:31 -0400

    console app

---
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class ConsoleApp
+{  
+	 static void Main()
+	{
+		string k = "kate";
+		string va = "virginia";
+
+		DateTime today = DateTime.Today;
+
+		DateTime daysLeft = DateTime.Parse("12/25/2020");
+
+		//Calculate countdown timer.
+		TimeSpan t = daysLeft - today;
+
+		Console.WriteLine("My name is {0}.");
+		Console.WriteLine("I am from {1}.");
+		Console.WriteLine("Today is" + today.ToString("dd/MM/yyyy"));
+		Console.WriteLine("There are " + t + " days until Christmas");
+
+		double width, height, woodLength, glassArea;
+		string widthString, heightString;
+
+		widthString = Console.ReadLine();
+		width = double.Parse(widthString);
+
+		heightString = Console.ReadLine();
+
+		height = double.Parse(heightString);
+
+		woodLength = 2 * (width + height) * 3.25;
+		glassArea = 2 * (width * height);
+
+		Console.WriteLine("The length of the wood is " +
+		woodLength + " feet");
+
+		Console.WriteLine("The area of the glass is " +
+		glassArea + " square metres");
+
+
+
+		Console.ReadKey();
+	}
+}
